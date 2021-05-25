873da73e940a5f845e45162dc8ceb354ba36bc0b
Isaac Lopez <isaac.lopez@upr.edu>
2021-05-25 00:53:21 -0400

    Finished Parsing data

--- app/Election.hs
+++ app/Election.hs
@@ -3,22 +3,23 @@ module Election ( runElection
                  ) where
 
 import           DataStructures (Candidate (..))
-import           ElectionUtil   (ballotParser, toTuple)
+import           ElectionUtil   (ballotParser, convert, createCandidates,
+                                 makeBallotsIntegers, toTuple)
 import           Parser         (getCSVContents, toList)
 
 runElection :: FilePath -> FilePath -> IO ()
 runElection candidateFile ballotsFile = do
     candidatesContents  <- getCSVContents candidateFile
     ballotsContents     <- getCSVContents ballotsFile
-    let candidates = organizeCandidates candidatesContents -- set the string from the file to list of strings
     let ballots = organizeBallots ballotsContents -- set the string from the file to list of strings
-    let cand1 = Candidate (fst $ head candidates) (snd $ head candidates)
+    let candidates = createCandidates $ organizeCandidates candidatesContents -- set the string from the file to list of strings
     print ballots
+
 organizeCandidates :: String -> [(String,Int)]
 organizeCandidates candidates = toTuple $ toList candidates
 
-organizeBallots :: String -> [[String]]
-organizeBallots ballots = map (ballotParser (==':')) (toList ballots)
+organizeBallots :: String -> [(Int, [(Int,Int)])]
+organizeBallots ballots = convert $ makeBallotsIntegers $ map (ballotParser (==':')) (toList ballots)
 
---countVoteNumber :: Int -> Candidate -> Int
---countVoteNumber vote candidate = length (filter (==vote) votes candidate)
+
+
--- src/ElectionUtil.hs
+++ src/ElectionUtil.hs
@@ -1,8 +1,12 @@
 module ElectionUtil
   ( toTuple
   , ballotParser
+  , createCandidates
+  , convert
+  , makeBallotsIntegers
   ) where
 
+import           DataStructures (Candidate (..))
 toTuple :: [String] -> [(String, Int)]
 toTuple = makeTuple
   where
@@ -10,10 +14,23 @@ toTuple = makeTuple
     makeTuple (x1:x2:xs) = (x1, (read x2::Int)):makeTuple xs
     makeTuple _          = []
 
+convert :: Foldable t => t [a] -> [(a, [(a, a)])]
+convert = snd . foldr cons nil
+        where nil = ([],[])
+              cons [x,y] (z,w) = ((x,y):z,w)
+              cons [x] (y,z)   = ([], (x,y):z)
+
 ballotParser :: (Char -> Bool) -> String -> [String]
 ballotParser p s = case dropWhile p s of
                 "" -> []
                 s' -> w : ballotParser p s''
                       where (w, s'') = break p s'
 
---createCandidate :: [String] -> Candidate
+makeBallotsIntegers :: [[String]] -> [[Int]]
+makeBallotsIntegers xs = map (\x -> map (\y -> read y :: Int) x) xs
+
+createCandidate :: (String,Int) -> Candidate
+createCandidate tuple = Candidate (fst tuple) (snd tuple)
+
+createCandidates :: [(String, Int)] -> [Candidate]
+createCandidates list = map createCandidate list
